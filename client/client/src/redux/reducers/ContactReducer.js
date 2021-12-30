e1b428b90e6ded874a097cbd210259c69f21df52
Sami BELHAJ <92115378+samibelhaj92@users.noreply.github.com>
2021-12-30 09:30:02 +0100

    updating contactDetails

--- client/client/src/redux/reducers/ContactReducer.js
+++ client/client/src/redux/reducers/ContactReducer.js
@@ -12,7 +12,7 @@ const ContactReducer=(state=initialState,action)=>{
         
         case GET_CONTACTS: return {...state, contacts: action.payload.CollectionContact}
         
-        case GET_CONTACT: return {...state, contact: action.payload.CollectionContact}
+        case GET_CONTACT: return {...state, contact: action.payload.foundContact}
 
         case TOGGLE_TRUE: return {...state, edit:true}
  
